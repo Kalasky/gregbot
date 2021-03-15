17a5a8e0bd852877e48d4b177614c7b028697cbd
Robert Kalasky <kalaskyr@gmail.com>
2021-03-14 20:01:29 -0400

    initial commit (basic setup)

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const tmi = require("tmi.js");
+
+const options = {
+  options: { debug: true },
+  connection: {
+    cluster: "aws",
+    reconnect: true,
+  },
+  identity: {
+    username: "Gregs_WZ_Stats",
+    password: process.env.AUTH_TOKEN,
+  },
+  channels: ["kalaskyyy"],
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on("connected", (address, port) => {
+  client.action("kalaskyyy", "connected");
+});
+
+client.on("chat", (channel, user, message, self) => {
+  if (message === "!game") {
+    client.action("kalaskyyy", "yo");
+  }
+});
