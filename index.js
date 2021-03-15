c2816fcca1504745665c39d36cdb5020764a6b1d
Robert Kalasky <kalaskyr@gmail.com>
2021-03-15 04:03:22 -0400

    chess stats commands

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const tmi = require("tmi.js");
 const API = require("call-of-duty-api")();
+const { prefix } = require("./config.json");
+const ChessWebAPI = require("chess-web-api");
+const chessAPI = new ChessWebAPI();
 
 const options = {
   options: { debug: true },
@@ -20,10 +23,70 @@ const client = new tmi.client(options);
 client.connect();
 
 client.on("connected", (address, port) => {
-  client.action("GregTheBoomer", "connected");
+  console.log("connected");
 });
 
 client.on("chat", (channel, user, message, self) => {
+  // If the message doesn't start with the prefix, exit early.
+  if (!message.startsWith(prefix)) return;
+
+  switch (message) {
+    case ">commands":
+      client.action(
+        "GregTheBoomer",
+        `Here's a list of my commands: https://pastebin.com/V8Uv4AcH`
+      );
+      break;
+    case ">help":
+      client.action(
+        "GregTheBoomer",
+        `Here's a list of my commands: https://pastebin.com/V8Uv4AcH`
+      );
+      break;
+    case ">rapidchess":
+      chessAPI.getPlayerStats("GregTheBoomer").then(
+        function (response) {
+          let rapidWin = response.body.chess_rapid.record.win;
+          let rapidLoss = response.body.chess_rapid.record.loss;
+          let rapidDraw = response.body.chess_rapid.record.draw;
+          let rapidBestRating = response.body.chess_rapid.best.rating;
+          let rapidCurrentRating = response.body.chess_rapid.last.rating;
+          client.action(
+            "GregTheBoomer",
+            `Greg's stats in Rapid: Wins: ${rapidWin} Losses: ${rapidLoss} Draws: ${rapidDraw} Current Elo: ${rapidCurrentRating} Best Elo: ${rapidBestRating}`
+          );
+        },
+        function (err) {
+          console.error(err);
+        }
+      );
+      break;
+    case ">blitzchess":
+      chessAPI.getPlayerStats("GregTheBoomer").then(
+        function (response) {
+          let blitzWin = response.body.chess_blitz.record.win;
+          let blitzLoss = response.body.chess_blitz.record.loss;
+          let blitzDraw = response.body.chess_blitz.record.draw;
+          let blitzBestRating = response.body.chess_blitz.best.rating;
+          let blitzCurrentRating = response.body.chess_blitz.last.rating;
+          client.action(
+            "GregTheBoomer",
+            `Greg's stats in Blitz: Wins: ${blitzWin} Losses: ${blitzLoss} Draws: ${blitzDraw} Current Elo: ${blitzCurrentRating} Best Elo: ${blitzBestRating}`
+          );
+        },
+        function (err) {
+          console.error(err);
+        }
+      );
+      break;
+
+    default:
+      client.action(
+        "GregTheBoomer",
+        `Invalid command. Check out a list of valid commands here: https://pastebin.com/V8Uv4AcH`
+      );
+  }
+
   API.login(process.env.COD_EMAIL, process.env.COD_PASSWORD).then(() => {
     const id = "GregSC#21708";
     const platform = "battle";
@@ -34,10 +97,6 @@ client.on("chat", (channel, user, message, self) => {
         let wins = wzData.br.wins;
         let topTen = wzData.br.topTen;
         let topFive = wzData.br.topFive;
-
-        // if (message === ">wins") {
-        //   client.action("GregTheBoomer", `Greg has ${expr} wins in Warzone`);
-        // }
 
         switch (message) {
           case ">kills":
@@ -69,7 +128,7 @@ client.on("chat", (channel, user, message, self) => {
             break;
 
           default:
-            console.log("invalid command");
+            console.log("Invalid command");
         }
       })
       .catch((err) => {
